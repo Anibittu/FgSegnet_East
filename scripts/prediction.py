68f719c8f6099834c08439e95658851babcd3010
Anibittu <anish.kumar.min17@itbhu.ac.in>
2020-08-12 13:55:08 +0530

    pred

--- scripts/prediction.py
+++ scripts/prediction.py
@@ -16,7 +16,7 @@ def load_image(path):
     x = np.expand_dims(x, axis=0)
     return x
 
-image_path = '/home/ug2017/min/17155014/inputx/733.png'
+image_path = '/home/ug2017/min/17155014/inputx/109.png'
 
 model_path = '/home/ug2017/min/17155014/weights/weightrr00000015.h5'
 
@@ -36,7 +36,7 @@ plt.imshow(probs)
 
 plt.title('Segmentation mask before thresholding')
 plt.axis('off')
-plt.savefig('733coc15.png')
+plt.savefig('109east15.png')
 plt.show()
 
 threshold = 0.8
@@ -51,5 +51,81 @@ plt.imshow(probs)
 
 plt.title('Segmentation mask after thresholding')
 plt.axis('off')
-plt.savefig('733COC15.png')
+plt.savefig('109east15.png')
+plt.show()
+
+
+
+model_path = '/home/ug2017/min/17155014/weights/weightconcat00000015.h5'
+
+x = load_image(image_path) # load a test frame
+model = load_model(model_path, custom_objects={'MyUpSampling2D': MyUpSampling2D, 'InstanceNormalization': InstanceNormalization, 'loss':loss, 'acc':acc, 'loss2':loss2, 'acc2':acc2}) #load the trained model
+probs = model.predict(x, batch_size=1, verbose=1)
+print(probs.shape) # (1, 240,320,1)
+probs = probs.reshape([probs.shape[1], probs.shape[2]])
+print(probs.shape) # (240,320)
+
+
+plt.subplot(1, 1, 1)
+plt.rcParams['figure.figsize'] = (5.0, 5.0)
+plt.rcParams['image.cmap'] = 'gray'
+
+plt.imshow(probs)
+
+plt.title('Segmentation mask before thresholding')
+plt.axis('off')
+plt.savefig('109add15.png')
+plt.show()
+
+threshold = 0.8
+probs[probs<threshold] = 0.
+probs[probs>=threshold] = 1.
+
+plt.subplot(1, 1, 1)
+plt.rcParams['figure.figsize'] = (5.0, 5.0)
+plt.rcParams['image.cmap'] = 'gray'
+
+plt.imshow(probs)
+
+plt.title('Segmentation mask after thresholding')
+plt.axis('off')
+plt.savefig('109ADD15.png')
+plt.show()
+
+
+
+model_path = '/home/ug2017/min/17155014/weights/weightfad00000015.h5'
+
+x = load_image(image_path) # load a test frame
+model = load_model(model_path, custom_objects={'MyUpSampling2D': MyUpSampling2D, 'InstanceNormalization': InstanceNormalization, 'loss':loss, 'acc':acc, 'loss2':loss2, 'acc2':acc2}) #load the trained model
+probs = model.predict(x, batch_size=1, verbose=1)
+print(probs.shape) # (1, 240,320,1)
+probs = probs.reshape([probs.shape[1], probs.shape[2]])
+print(probs.shape) # (240,320)
+
+
+plt.subplot(1, 1, 1)
+plt.rcParams['figure.figsize'] = (5.0, 5.0)
+plt.rcParams['image.cmap'] = 'gray'
+
+plt.imshow(probs)
+
+plt.title('Segmentation mask before thresholding')
+plt.axis('off')
+plt.savefig('109coc15.png')
+plt.show()
+
+threshold = 0.8
+probs[probs<threshold] = 0.
+probs[probs>=threshold] = 1.
+
+plt.subplot(1, 1, 1)
+plt.rcParams['figure.figsize'] = (5.0, 5.0)
+plt.rcParams['image.cmap'] = 'gray'
+
+plt.imshow(probs)
+
+plt.title('Segmentation mask after thresholding')
+plt.axis('off')
+plt.savefig('109COC15.png')
 plt.show()
